346b50e512e6df1c372070096ea8a9f7cb2e7168
vijaya-l <vijaya.ca@outlook.com>
2022-04-11 21:42:34 -0400

    Day 1 assignments

---
+++ Day 001/swap_numbers.py
@@ -0,0 +1,23 @@
+# Write a program that switches the values stored in the variables a and b.
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+# Write your code below this line 👇
+
+# use a temp variable c
+c = a
+a = b
+b = c
+
+# another way to do this is a, b = b, a
+
+# Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
