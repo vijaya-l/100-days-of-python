b2f9afa47fd35a3a287c68926311a7726a69092f
vijaya-l <vijaya.ca@outlook.com>
2022-04-14 17:25:21 -0400

    Day 4 inprogress

---
+++ Day 004/count.py
@@ -0,0 +1,29 @@
+# To work out the love score between two people:
+
+# > Take both people's names and check for the number of times the letters in the word TRUE occurs. Then check for the number of times the letters in the word LOVE occurs. Then combine these numbers to make a 2 digit number.
+
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+
+name = name1 + name2
+
+lower_name = name.lower()
+
+t = lower_name.count("t")
+r = lower_name.count("r")
+u = lower_name.count("u")
+e = lower_name.count("e")
+
+true = t + r + u + e
+
+l = lower_name.count("l")
+o = lower_name.count("o")
+v = lower_name.count("v")
+e = lower_name.count("e")
+
+love = l + o + v + e
+
+true_love = str(true) + str(love)
+
+print(true_love)
