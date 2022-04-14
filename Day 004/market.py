182e8c3a8d8f704816cce8d9ca99dd4e112f1249
vijaya-l <vijaya.ca@outlook.com>
2022-04-14 18:39:35 -0400

    Day 004 is completed

---
+++ Day 004/market.py
@@ -0,0 +1,17 @@
+# if you want to pick fruit market[0][0,1,2...]
+# if you want to pick fruit market[1][0,1,2...]
+
+fruits = [
+    "Strawberries",
+    "Nectarines",
+    "Apples",
+    "Grapes",
+    "Peaches",
+    "Cherries",
+    "Pears",
+]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+market = [fruits, vegetables]
+
+print(market[1][2])
