ee921ecd111a56a916d8fe995abfa450fbf353f3
vijaya-l <vijaya.ca@outlook.com>
2022-04-16 18:31:04 -0400

    Day 006 in progress

---
+++ Day 006/function.py
@@ -0,0 +1,12 @@
+# define a 'function' as wish and call inside 'while' loop
+def wish():
+    print("Vijaya")
+
+
+# for welcome in range(6):
+#     wish()
+
+count = 5
+while count > 0:
+    count -= 1
+    wish()
