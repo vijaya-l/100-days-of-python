9d9fe8350878bb93186f590ce1c9a5cba5f1e743
vijaya-l <vijaya.ca@outlook.com>
2022-04-21 14:23:22 -0400

    Day 10 inprogress

---
+++ Day 010/dictionary.py
@@ -0,0 +1,15 @@
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+}
+# add new items to dictionary
+programming_dictionary["code"] = "programming like java,Python"
+
+# empty dictionary
+# programming_dictionary={}
+
+print(programming_dictionary)
+
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
